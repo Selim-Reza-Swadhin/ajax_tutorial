95912c11beb26035c5a179a2d3391a387e2ec1bf
Selim Reza Swadhin <selimrezaswadhin@gmail.com>
2020-09-01 15:20:36 +0600

    initial commit

---
+++ Content_load/page.php
@@ -0,0 +1,19 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    echo $i . '<br/>';
+}
+?>
+<style>
+    img {
+        margin-left: 362px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+</style>
+
+<h1>This has been loaded into another page</h1>
+<img src="vig.jpg" width="600" height="450" alt="">
+<br><br>
+
